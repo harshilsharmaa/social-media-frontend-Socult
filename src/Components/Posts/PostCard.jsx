5a933f51e871798a2fae4d3ad7b5091c65d80ec6
HARSHIL SHARMA <harshilsharmaa0@gmail.com>
2023-06-18 10:47:33 +0000

    Add comment, like in post

--- src/Components/Posts/PostCard.jsx
+++ src/Components/Posts/PostCard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './PostCard.css'
+import { Link } from 'react-router-dom/dist';
+import { likeIt } from './PostInfo';
 
 const PostCard = ({item}) => {
+
+  const [likes, setLikes] = useState(item.likes);
+  const [isLiked , setIsLiked] = useState(false);
+
+  const likeHandler = (e)=>{
+    e.preventDefault();
+    if(!isLiked){
+      setLikes(likes+1);
+      setIsLiked(true);
+      likeIt(item.id);
+    }
+  }
+
+
+
   return (
     <div className='postCard'>
       <div className="authorInfo">
@@ -16,7 +33,8 @@ const PostCard = ({item}) => {
         <img src={item.media} alt="" />
       </div>
       <div className="likes">
-        Likes: {item.likes}
+        Likes: {likes}
+        <button onClick={(e)=>likeHandler(e)}>like</button>
       </div>
       <div className="description">{item.description}</div>
       <div className="comments-box">
@@ -28,7 +46,7 @@ const PostCard = ({item}) => {
                 <div key={index} className="comment">
                   <div className="authorInfo">
                     <img src={comment.authorImage} alt="" />
-                    <p>{comment.authorName}</p>
+                    <Link style={{textDecoration:'none',color:'blue', marginLeft:'5px'}}>{comment.authorName}</Link>
                   </div>
                   <div className="comment-content">
                     <p>{comment.comment}</p>
@@ -37,6 +55,10 @@ const PostCard = ({item}) => {
               )
             })
           }
+          <div className="input">
+            <input type="text" placeholder='Post a comment'/>
+            <button>Post</button>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Posts/PostInfo.js
+++ src/Components/Posts/PostInfo.js
@@ -1,9 +1,36 @@
-exports.postInfo = [
+export const postInfo = [
     {
         id: 1,
         authorImage: "https://img.freepik.com/free-photo/portrait-happy-young-woman-looking-camera_23-2147892777.jpg?w=2000",
         authorName: "Jerry",
         likes: 34,
+        isLiked: false,
+        media: "https://media.istockphoto.com/id/898926286/photo/cute-girl.jpg?s=170667a&w=0&k=20&c=QIP2aOV6Z33HhV4D44AZ8cuwzgKt3FlM1BLiOO8dAuw=",
+        description: "asf aslf alsfka efkaslf erklflaef kasnflnasmlfmlas lerlamef klmnelmasfeelaw lamelm ajjemfoqmwef lmmefpqwm mfmlqwemf pqewjfmq pjmlm",
+        comments: [
+            {
+                authorImage: "https://img.freepik.com/free-photo/portrait-happy-young-woman-looking-camera_23-2147892777.jpg?w=2000",
+                authorName: "abcd",
+                comment: "sdf, alkfl alksf ads lsd"
+            },
+            {
+                authorImage: "https://img.freepik.com/free-photo/portrait-happy-young-woman-looking-camera_23-2147892777.jpg?w=2000",
+                authorName: "abcd",
+                comment: "sdf, alkfl alksf ads lsd"
+            },
+            {
+                authorImage: "https://img.freepik.com/free-photo/portrait-happy-young-woman-looking-camera_23-2147892777.jpg?w=2000",
+                authorName: "abcd",
+                comment: "sdf, alkfl alksf ads lsd"
+            }
+        ]
+    },
+    {
+        id: 2,
+        authorImage: "https://img.freepik.com/free-photo/portrait-happy-young-woman-looking-camera_23-2147892777.jpg?w=2000",
+        authorName: "Jerry",
+        likes: 34,
+        isLiked: false,
         media: "https://media.istockphoto.com/id/898926286/photo/cute-girl.jpg?s=170667a&w=0&k=20&c=QIP2aOV6Z33HhV4D44AZ8cuwzgKt3FlM1BLiOO8dAuw=",
         description: "asf aslf alsfka efkaslf erklflaef kasnflnasmlfmlas lerlamef klmnelmasfeelaw lamelm ajjemfoqmwef lmmefpqwm mfmlqwemf pqewjfmq pjmlm",
         comments: [
@@ -25,3 +52,15 @@ exports.postInfo = [
         ]
     }
 ]
+
+export const likeIt = (postId)=>{
+    console.log(postId)
+    const post = postInfo.find((item)=>{
+        item.id==postId
+    });
+    // postInfo.map((item)=>{
+    //     console.log(item.id)
+    // });
+
+    console.log(post);
+}
